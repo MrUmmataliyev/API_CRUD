9b70c41b4b79f62f3d26b13e71e130743a704b87
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-02-14 14:59:49 +0500

    ADO Post & Put

--- WebApplication1/Controllers/UsersController.cs
+++ WebApplication1/Controllers/UsersController.cs
@@ -70,5 +70,62 @@ namespace WebApplication1.Controllers
                 return $"Delete status [=> {status}";
             }
         }
+
+        [HttpGet]
+        public List<Products> GetADO()
+        {
+            using(NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING))
+            {
+                string query = "select * from products";
+                connection.Open();
+                NpgsqlCommand command = new NpgsqlCommand(query, connection);
+                var res = command.ExecuteReader();
+                List<Products>? products = new List<Products>();
+                while(res.Read()) { 
+
+                 products.Add(new Products
+                 {
+                     Id = (int)res[0],
+                     Name = (string)res[1],
+                     Description = (string)res[2],
+                     PhotoPath = (string)res[3]
+                 });
+
+                }
+                return products;
+            }
+        }
+        [HttpPost]
+        public string PostADO(string name, string description, string PhotoPath)
+        {
+            using(NpgsqlConnection connection=new NpgsqlConnection(CONNECTIONSTRING))
+            {
+                string query = $"insert into products(name, description, photopath) values (@name, @description, @photopath)";
+                connection.Open();
+                NpgsqlCommand command = new NpgsqlCommand( query, connection);
+                command.Parameters.AddWithValue("name", name);
+                command.Parameters.AddWithValue("description", description);
+                command.Parameters.AddWithValue("photopath", PhotoPath);
+                int status = command.ExecuteNonQuery();
+                return $"Post status [=> {status}";
+
+            }
+        }
+        [HttpPut]
+        public string PutADO(int id, string name, string description, string photopath)
+        {
+            using(NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING))
+            {
+                string query = $"update products set name = @name, description = @description, photopath = @photopath where id = @id";
+                connection.Open();
+                NpgsqlCommand command = new NpgsqlCommand(query, connection);
+                command.Parameters.AddWithValue("id", id);
+                command.Parameters.AddWithValue("name", name); 
+                command.Parameters.AddWithValue("description", description);
+                command.Parameters.AddWithValue("photopath", photopath);
+                int status = command.ExecuteNonQuery();
+                return $"Put status [=> {status}";
+            }
+        }
     }
 }
